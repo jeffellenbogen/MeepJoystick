4f2734e5a670b494bbd4d471c8ab6ca6d15a3887
gsalaman <43499190+gsalaman@users.noreply.github.com>
2018-10-08 12:10:10 -0600

    Add first cut ack/resend code.
    Currently just does the "direction" commands.

--- MeepJoystick.ino
+++ MeepJoystick.ino
@@ -1,6 +1,14 @@
 // SoftwareSerial is used to communicate with the XBee
 #include <SoftwareSerial.h>
 
+#if !defined (TRUE)
+#define TRUE 1
+#endif
+
+#if !defined (FALSE)
+#define FALSE 0
+#endif
+
 SoftwareSerial XBee(2, 3); // Arduino RX, TX (XBee Dout, Din)
 
 #define zippyy_switch_pin_1 6   //white     
@@ -22,6 +30,83 @@ int tempDirection = 5;
 int Speed = 2;
 int Direction;
 
+typedef enum
+{
+  DIR_INVALID    = 0,
+  DIR_FWD_LEFT   = 1,
+  DIR_FWD        = 2,
+  DIR_FWD_RIGHT  = 3,
+  DIR_LEFT       = 4,
+  DIR_STOP       = 5,
+  DIR_RIGHT      = 6,
+  DIR_BACK_LEFT  = 7,
+  DIR_BACK       = 8,
+  DIR_BACK_RIGHT = 9
+} dir_type;
+
+char *drive_string[] =
+{
+  "INVALID drive!",
+  "Drive Forward Left",
+  "Drive Forward",
+  "Drive Forward Right",
+  "Drive Left",
+  "Stop Driving",
+  "Drive Right",
+  "Drive Back Left",
+  "Drive Back"
+  "Drive Back Right"
+};
+
+typedef enum
+{
+  SPEED_INVALID = 0,
+  SPEED_SLOW_MO = 1,
+  SPEED_REGULAR = 2,
+  SPEED_TURBO   = 3
+} speed_type;
+
+static dir_type joystick_dir=DIR_STOP;  // current joystick orientation.
+
+typedef enum
+{
+  RX_IDLE,
+  WAIT_FOR_CMD,
+  WAIT_FOR_DATA
+} parse_state_type;
+
+typedef struct
+{
+  dir_type         last_sent_dir;
+  bool             last_dir_acked;
+  unsigned long    last_sent_dir_time;
+  speed_type       last_sent_speed;
+  bool             last_speed_acked;
+  unsigned long    last_sent_speed_time;
+  parse_state_type parse_state;
+  
+} ack_state_type;
+
+ack_state_type ack_state;
+
+#define ACK_TIMEOUT   1000  // 250 ms for an ack to timeout before resending.
+
+#define ACK_CMD 'A'
+
+
+void init_ack_state( void )
+{
+  ack_state.last_sent_dir = DIR_STOP;
+  ack_state.last_sent_dir_time = 0;
+  ack_state.last_dir_acked = TRUE;
+
+  ack_state.last_sent_speed = SPEED_REGULAR;
+  ack_state.last_sent_speed_time = 0;
+  ack_state.last_speed_acked = TRUE;
+
+  ack_state.parse_state = RX_IDLE;
+}
+
 void setup() 
 {  
   Serial.begin(9600); // Start serial communication
@@ -39,32 +124,10 @@ void setup()
   analogWrite(LED_pinR, 210);
   analogWrite(LED_pinG, 150); 
   analogWrite(LED_pinB, 0);
-}
 
-void old_read_joystick_direction(void)
-{
-      if ((digitalRead(zippyy_switch_pin_4) == 0) && 
-      (digitalRead(zippyy_switch_pin_2) == 0))
-        Direction = 3;
-      else if ((digitalRead(zippyy_switch_pin_1) == 0) && 
-      (digitalRead(zippyy_switch_pin_2) == 0))
-        Direction = 9;
-      else if ((digitalRead(zippyy_switch_pin_1) == 0) && 
-      (digitalRead(zippyy_switch_pin_3) == 0))
-        Direction = 7;
-      else if ((digitalRead(zippyy_switch_pin_3) == 0) && 
-      (digitalRead(zippyy_switch_pin_4) == 0))
-        Direction = 1;    
-      else if(digitalRead(zippyy_switch_pin_1) == 0)
-        Direction = 8;
-      else if(digitalRead(zippyy_switch_pin_4) == 0)
-        Direction = 2;
-      else if(digitalRead(zippyy_switch_pin_2) == 0)
-        Direction = 6;
-      else if(digitalRead(zippyy_switch_pin_3) == 0)
-        Direction = 4;
-      else
-        Direction = 5;  
+  init_ack_state();
+
+  Serial.println("Joystick setup");
 }
 
 void read_joystick_direction(void)
@@ -82,20 +145,122 @@ void read_joystick_direction(void)
 
   if (up)
   {
-    if (left)       Direction = 1;
-    else if (right) Direction = 3;
-    else            Direction = 2;
+    if (left)       Direction = DIR_FWD_LEFT;
+    else if (right) Direction = DIR_FWD_RIGHT;
+    else            Direction = DIR_FWD;
   }
   else if (down)
   {
-    if (left)       Direction = 7;
-    else if (right) Direction = 9;
-    else            Direction = 8;
+    if (left)       Direction = DIR_BACK_LEFT;
+    else if (right) Direction = DIR_BACK_RIGHT;
+    else            Direction = DIR_BACK;
   }
-  else if (left)    Direction = 4;
-  else if (right)   Direction = 6;
-  else              Direction = 5;
+  else if (left)    Direction = DIR_LEFT;
+  else if (right)   Direction = DIR_RIGHT;
+  else              Direction = DIR_STOP;
+}
+
+void send_direction(dir_type dir)
+{
+  char *serial_string;
   
+  ack_state.last_sent_dir = dir;
+  ack_state.last_dir_acked = FALSE;
+  ack_state.last_sent_dir_time = millis();
+  ack_state.parse_state = WAIT_FOR_CMD;
+
+  // For some reason, Arduino didn't like:
+  //   Serial.println(drive_string[dir]);
+  // Some weird macro substitution thing going on maybe?
+  serial_string = drive_string[dir];
+  Serial.println(serial_string); 
+  
+  XBee.print(dir);
+}
+
+void check_for_resends( void )
+{
+  unsigned int current_time;
+
+  current_time = millis();
+
+  // do we have an outstanding direction command?
+  if (ack_state.last_dir_acked == FALSE)
+  {
+    // is it time to resend?
+    if (current_time > ack_state.last_sent_dir_time + ACK_TIMEOUT)
+    {
+      Serial.println("RESEND!");
+      send_direction(ack_state.last_sent_dir);
+    }
+  }
+
+  // do we have an outstanding speed command?
+  // currently unimplemented...
+}
+
+void check_for_acks( void )
+{
+  char rx_char;
+
+  while (XBee.available())
+  {
+    rx_char = XBee.read();
+
+    Serial.print("*** MEEP: ");
+    Serial.println(rx_char);
+    
+    switch (ack_state.parse_state)
+    {
+      case WAIT_FOR_CMD:
+        // in this state, we're waiting on the "ack" command.
+        if (rx_char == ACK_CMD)
+        {
+          ack_state.parse_state = WAIT_FOR_DATA;      
+        }
+        else
+        {
+          // got something other than an "ack" command, and that's the only 
+          // thing currently impelemented.  Flag the unexpected input.
+          Serial.print("Unexpected data over XBee whilst waiting for Ack: ");
+          Serial.println(rx_char);
+        }
+      break;
+
+      case WAIT_FOR_DATA:
+        // in this state, we're waiting for either the direction or speed character
+
+        // An annoying bit:  the received character is...wait for it...a character.
+        // The "last_sent_dir" is an integer.  Need to convert to compare...hence the +'0'
+        if (rx_char == ack_state.last_sent_dir + '0')
+        {
+          ack_state.last_dir_acked = TRUE;
+          ack_state.parse_state = RX_IDLE;
+        }
+        else if (rx_char == ack_state.last_sent_speed)
+        {
+          ack_state.last_speed_acked = TRUE;
+          ack_state.parse_state = RX_IDLE;
+        }
+        else
+        {
+           // this ack didn't match either our last sent speed or dir.
+           // Flag the unexpected input...and that means we're waiting for a full cmd again.
+           Serial.print("Unexpeced ack from MEEP: ");
+           Serial.println(rx_char);
+           ack_state.parse_state = WAIT_FOR_CMD;
+        }
+      break;
+
+      case RX_IDLE:
+        // In this state, we had no outstanding command to the meep, so we're not
+        // expecting anything back.
+        Serial.print("Unexpected data whilst idle: ");
+        Serial.println(rx_char);
+      break;
+        
+    }
+  }
 }
 
 void loop() 
@@ -104,85 +269,27 @@ void loop()
   //Read ZIPPYY Joystick
   read_joystick_direction();
        
-  delay (100);
-       
-//Check to see if joystick direction has changed
+  // Has our joystick direction changed?
+  if (Direction != ack_state.last_sent_dir)
+  {
+    // send direction, and mark the current time.
+    send_direction(Direction);
+  }
 
-      if(Direction != tempDirection){
-         if (Direction == 1)
-            driveForwardSlightLeft();
-         else if (Direction == 2)
-            driveForward();
-         else if (Direction == 3)
-            driveForwardSlightRight();           
-         else if (Direction == 4)
-            driveLeft();
-         else if (Direction == 5)
-            stopDriving();        
-         else if (Direction == 6)
-            driveRight();     
-         else if (Direction == 7)
-            driveBackSlightLeft();
-         else if (Direction == 8)
-            driveBack();       
-         else if (Direction == 9)
-            driveBackSlightRight();       
-           
-         tempDirection = Direction;
-      }
+  // Speed not currenly being acked...add soon.
+  if (digitalRead(speedButton_pin) == 0)
+  {
+    speedToggle();
+  }
 
-      if (digitalRead(speedButton_pin) == 0){
-        speedToggle();
-        delay(100);
-      }
-         
-      
+  check_for_acks();
+  check_for_resends();
+
+  // this delay is going to act as a temporary debounce...
+  delay(100);
+  
 }
 
-void stopDriving(){
-  Serial.println("Stop");
-  XBee.print('5');
-}
-
-void driveForward(){
-  Serial.println("Drive Forward");
-  XBee.print('2');
-}
-
-void driveBack(){
-  Serial.println("Drive Back");
-  XBee.print('8');
-}
-
-void driveLeft(){
-  Serial.println("Drive Left");
-  XBee.print('4');
-}
-
-void driveRight(){
-  Serial.println("Drive Right");
-  XBee.print('6');
-}
-
-void driveForwardSlightLeft(){
-  Serial.println("Drive Forward Slight Left");
-  XBee.print('1');
-}
-
-void driveForwardSlightRight(){
-  Serial.println("Drive Forward Slight Right");
-  XBee.print('3');
-}
-
-void driveBackSlightRight(){
-  Serial.println("Drive Back Slight Right");
-  XBee.print('9');
-}
-
-void driveBackSlightLeft(){
-  Serial.println("Drive Back Slight Left");
-  XBee.print('7');
-}
 
 void speedToggle(){
 
